dccd30087cfa1150b0bb88cb2b11a2385bb46bb1
koosco <rnxogud123@naver.com>
2024-05-28 01:31:03 +0900

    feat: BasicDriverBuilder 추가

---
+++ driver/basic/basic_driver_builder.py
@@ -0,0 +1,41 @@
+from basic.basic_driver_configurer import BasicDriverConfigurer
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+from basic.basic_driver import BasicDriver
+
+
+class BasicDriverBuilder(object):
+    def __init__(self):
+        self.configurer = BasicDriverConfigurer()
+
+    def headless(self, headless: bool):
+        self.configurer.headless = headless
+        return self
+
+    def no_sandbox(self, no_sandbox: bool):
+        self.configurer.no_sandbox = no_sandbox
+        return self
+
+    def has_pop_up(self, flag: bool):
+        self.configurer.pop_up = flag
+        return self
+
+    def configure(self):
+        chrome_options = Options()
+        if self.configurer.headless:
+            chrome_options.add_argument('--headless')
+        if self.configurer.no_sandbox:
+            chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_experimental_option('detach', True)
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+        driver = webdriver.Chrome(options=chrome_options)
+        if self.configurer.pop_up:
+            handles = driver.window_handles
+            for handle in handles:
+                if handle != handles[0]:
+                    driver.switch_to.window(handle)
+                    driver.close()
+            driver.switch_to.window(handles[0])
+        return BasicDriver(driver)
