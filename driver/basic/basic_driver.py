da5db1b96098f27191e5e8ef17d16233732a27ea
koosco <rnxogud123@naver.com>
2024-05-28 00:00:56 +0900

    feat: basic_driver 추가
    * srp를 위한 builder 패턴 사용
    * configurer에서 설정 -> driver 생성
    * driver는 scraping 로직만 수행

---
+++ driver/basic/basic_driver.py
@@ -0,0 +1,40 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import NoSuchElementException
+
+
+class WebDriver(object):
+    def __init__(self, driver):
+        self.driver = driver
+        self.body = None
+        self.page_source = None
+
+    def __del__(self):
+        self.driver.quit()
+        pass
+
+    def open_url(self, url: str):
+        self.driver.get(url)
+        self.page_source = self.driver.page_source
+
+    def get_page_source(self):
+        return self.page_source
+
+    def save_page_source(self, path: str):
+        with open(path, 'w') as file:
+            for line in self.page_source:
+                file.write(line)
+        print('page source saved')
+
+    def click(self, path: str):
+        button = (WebDriverWait(self.driver, 10).
+                  until(ec.presence_of_element_located((By.XPATH, path))))
+        print(button.text)
+        button.click()
+
+    def find_by_xpath(self, path: str):
+        try:
+            return self.driver.find_element(By.XPATH, path)
+        except NoSuchElementException:
+            return None
