69c09ff7c8f1cde3ff0e3ad11deb4c098c2fb76c
koosco <rnxogud123@naver.com>
2024-05-27 23:01:53 +0900

    feat: Item entity 추가

--- item_service/models.py
+++ item_service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    item_price = models.IntegerField()
+    item_image_url = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    option_name = models.CharField(max_length=100, default='')
+
+    class Meta:
+        db_table = 'item'
