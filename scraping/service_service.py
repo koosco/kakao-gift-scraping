5d443079c13ffd26c3b1ce2aa8d32ad712935beb
koosco <rnxogud123@naver.com>
2024-05-28 09:26:54 +0900

    feat: ScrapingService 추가

---
+++ scraping/service_service.py
@@ -0,0 +1,9 @@
+from driver.kakao.kakao_driver_builder import KakaoDriverBuilder
+
+
+class ScrapingService(object):
+    def __init__(self):
+        self.driver = KakaoDriverBuilder().configure()
+
+    def find_all(self):
+        pass
