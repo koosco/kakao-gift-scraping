4ba28816d1dab1906372f1f892dd7cece2546734
koosco <rnxogud123@naver.com>
2024-05-28 10:33:28 +0900

    refactor: KakaoDriverConfigurer 상속 변경
    * BasicDriverConfigurer를 상속받도록 변경

--- driver/kakao/kakao_driver_configurer.py
+++ driver/kakao/kakao_driver_configurer.py
@@ -1,3 +1,7 @@
-class KakaoDriverConfigurer(object):
+from basic.basic_driver_configurer import BasicDriverConfigurer
+
+
+class KakaoDriverConfigurer(BasicDriverConfigurer):
     def __init__(self):
+        super().__init__()
         self.url = 'https://gift.kakao.com/home'
