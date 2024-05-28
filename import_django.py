3eed650b67aae022efe2595ec4a46eb2da20abcd
koosco <rnxogud123@naver.com>
2024-05-28 16:01:38 +0900

    setting/fix: django app 설정 변경

--- import_django.py
+++ import_django.py
@@ -1,5 +1,5 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "job_scraping_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kakao_gift_scraping.settings")
 django.setup()
