e5f06717838d9ef0e79aa18279653ec5529d666e
koosco <rnxogud123@naver.com>
2024-05-28 10:32:55 +0900

    refactor: 상수명 변경

--- driver/kakao/const/class_name.py
+++ driver/kakao/const/class_name.py
@@ -1,12 +1,12 @@
 #  CATEGORY
-CATEGORY_LIST = 'txt_menu'
+CLASS_CATEGORY_LIST = 'txt_menu'
 
 #  ITEM_LIST
-ITEM_LIST = 'prdlist_pccol4'
-ITEM = 'cmp_prd'
+CLASS_ITEM_LIST = 'prdlist_pccol4'
+CLASS_ITEM = 'cmp_prd'
 
 #  ITEM
-ITEM_IMAGE_URL = 'img_thumb'
-ITEM_BRAND= 'txt_brand'
-ITEM_NAME = 'txt_prdname'
-ITEM_PRICE = 'num_price'
+CLASS_ITEM_IMAGE_URL = 'img_thumb'
+CLASS_ITEM_BRAND= 'txt_brand'
+CLASS_ITEM_NAME = 'txt_prdname'
+CLASS_ITEM_PRICE = 'num_price'
--- driver/kakao/kakao_driver.py
+++ driver/kakao/kakao_driver.py
@@ -12,7 +12,6 @@ PAGE_DOWN_CNT = 3
 
 class KakaoDriver(object):
     def __init__(self, driver: BasicDriver, item_builder):
-        super().__init__(driver)
         self._driver = driver
         self._item_builder = item_builder
 
@@ -26,7 +25,7 @@ class KakaoDriver(object):
         :return: None
         """
         category_size = self.get_first_category_size()  # first category 길이
-        for category_idx in range(1, category_size + 1):
+        for category_idx in range(1, category_size+1):
             sub_category_size = self.get_second_category_size(category_idx)  # second category 길이
             for sub_category_idx in range(sub_category_size):
                 sleep(SHORT_TIME)
@@ -34,7 +33,7 @@ class KakaoDriver(object):
                     # black list에 포함되지 않는 경우에만 다음 페이지를 조회
                     self.__find_items_in_page()  # item 목록을 찾는다
                 else:
-                    self._click(CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
+                    self._click(XPATH_CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
                 self._sub_category_name = None
             self._category_name = None
 
@@ -44,6 +43,7 @@ class KakaoDriver(object):
         :param category_idx: scrap할 페이지 idx
         :return: 카테고리에 포함된 항목들
         """
+        category_idx += 1
         sub_category_size = self.get_second_category_size(category_idx)  # second category 길이
         for sub_category_idx in range(sub_category_size):
             sleep(SHORT_TIME)
@@ -51,25 +51,26 @@ class KakaoDriver(object):
                 # black list에 포함되지 않는 경우에만 다음 페이지를 조회
                 return self.__find_items_in_page()  # item 목록을 찾는다
             else:
-                self._click(CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
+                self._click(XPATH_CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
 
     def scrap_sub_category(self, category_idx: int, sub_category_idx: int):
+        category_idx += 1
         sleep(SHORT_TIME)
         if not self.__is_in_black(category_idx, sub_category_idx):
             # black list에 포함되지 않는 경우에만 다음 페이지를 조회
             return self.__find_items_in_page()  # item 목록을 찾는다
         else:
-            self._click(CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
+            self._click(XPATH_CATEGORY_CANCEL_BUTTON)  # cateogory를 선택하지 못했다면 카테고리 선택 창을 닫는다
 
     def get_first_category_size(self):
         """
         첫 번째 카테고리 크기를 계산
         :return: 첫 번째 카테고리 크기
         """
-        self._click(CATEGORY_BUTTON)
-        res = len(self._driver._xpath(CATEGORY_LIST)
-                  .find_elements(By.CLASS_NAME, CATEGORY_LIST))
-        self._click(CATEGORY_CANCEL_BUTTON)
+        self._click(XPATH_CATEGORY_BUTTON)
+        res = len(self._driver._xpath(XPATH_CATEGORY_LIST)
+                  .find_elements(By.CLASS_NAME, CLASS_CATEGORY_LIST))
+        self._click(XPATH_CATEGORY_CANCEL_BUTTON)
         return res
 
     def get_second_category_size(self, category_idx: int):
@@ -78,30 +79,30 @@ class KakaoDriver(object):
         :param category_idx: 카테고리 인덱스
         :return: 두 번째 카테고리 크기
         """
-        self._click(CATEGORY_BUTTON)
-        self._click(CATEGORY_ELEMENT.format(i=category_idx))
-        sub_category_list = self._driver._xpath(SUB_LIST.format(i=category_idx - 1))
-        text_menus = sub_category_list.find_elements(By.CLASS_NAME, CATEGORY_LIST)
-        self._click(CATEGORY_CANCEL_BUTTON)
+        self._click(XPATH_CATEGORY_BUTTON)
+        self._click(XPATH_CATEGORY_ELEMENT.format(i=category_idx))
+        sub_category_list = self._driver._xpath(XPATH_SUB_LIST.format(i=category_idx - 1))
+        text_menus = sub_category_list.find_elements(By.CLASS_NAME, CLASS_CATEGORY_LIST)
+        self._click(XPATH_CATEGORY_CANCEL_BUTTON)
         return len(text_menus)
 
     def __is_in_black(self, category_idx: int, sub_category_idx: int) -> bool:
-        self._click(CATEGORY_BUTTON)  # category 버튼 클릭
+        self._click(XPATH_CATEGORY_BUTTON)  # category 버튼 클릭
 
         # first category 확인
-        category_name = self._driver._xpath(CATEGORY_ELEMENT.format(i=category_idx)).text
+        category_name = self._driver._xpath(XPATH_CATEGORY_ELEMENT.format(i=category_idx)).text
         if category_name in BLACK_LIST:
             return True
         self._category_name = category_name  # black list에 포함되지 않는다면 category_name을 설정
-        self._click(CATEGORY_ELEMENT.format(i=category_idx))  # black list에 포함되지 않는다면 category를 선택함
+        self._click(XPATH_CATEGORY_ELEMENT.format(i=category_idx))  # black list에 포함되지 않는다면 category를 선택함
 
         # second category 확인
-        sub_category_name = self._driver._xpath(SUB_CATEGORY_ELEMENT
+        sub_category_name = self._driver._xpath(XPATH_SUB_CATEGORY_ELEMENT
                                                 .format(i=category_idx - 1, j=sub_category_idx + 1)).text
         if sub_category_name in BLACK_LIST:
             return True
         self._sub_category_name = sub_category_name  # black list에 포함되지 않는다면 sub_category_name 설정
-        self._click(SUB_CATEGORY_ELEMENT.format(i=category_idx - 1, j=sub_category_idx + 1))  # black list에 포함되지 않는다면 sub category 선택
+        self._click(XPATH_SUB_CATEGORY_ELEMENT.format(i=category_idx - 1, j=sub_category_idx + 1))  # black list에 포함되지 않는다면 sub category 선택
         sleep(SHORT_TIME)
         return False
 
@@ -122,22 +123,22 @@ class KakaoDriver(object):
 
     def __find_items_in_page(self):
         items = []
-        self._click(ITEM_BUTTON)  # 페이지에 들어가면 item에 대한 항목으로 페이지 기준을 변경
+        self._click(XPATH_ITEM_BUTTON)  # 페이지에 들어가면 item에 대한 항목으로 페이지 기준을 변경
         self.__fill_category_builder()  # builder에 category, sub category 이름을 추가
         self.__page_down()
-        item_lists = self._driver.find_elements(By.CLASS_NAME, ITEM_LIST)  # 각각의 아이템 목록들 (추천 항목 제외)
+        item_lists = self._driver._class_name(CLASS_ITEM_LIST)  # 각각의 아이템 목록들 (추천 항목 제외)
         for item_list in item_lists:
-            elements = item_list.find_elements(By.CLASS_NAME, ITEM)  # 아이템 목록에서 아이템들을 찾음
+            elements = item_list.find_elements(By.CLASS_NAME, CLASS_ITEM)  # 아이템 목록에서 아이템들을 찾음
             self.__fill_category_builder()  # 카테고리 항목을 채우고
             for elem in elements:
                 items.append(self.__find_item(elem))  # 아이템을 dto로 만듬
         return items
 
     def __find_item(self, element):
-        item_image_url = element.find_element(By.CLASS_NAME, ITEM_IMAGE_URL).get_attribute('src')
-        brand_name = element.find_element(By.CLASS_NAME, ITEM_BRAND).text
-        item_name = element.find_element(By.CLASS_NAME, ITEM_NAME).text
-        price = element.find_element(By.CLASS_NAME, ITEM_PRICE).text
+        item_image_url = element.find_element(By.CLASS_NAME, CLASS_ITEM_IMAGE_URL).get_attribute('src')
+        brand_name = element.find_element(By.CLASS_NAME, CLASS_ITEM_BRAND).text
+        item_name = element.find_element(By.CLASS_NAME, CLASS_ITEM_NAME).text
+        price = element.find_element(By.CLASS_NAME, CLASS_ITEM_PRICE).text
 
         return (self._item_builder
                 .item_image_url(item_image_url)
