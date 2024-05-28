74491a8be71fee4ce0dac4dc2efd92115394b20f
koosco <rnxogud123@naver.com>
2024-05-28 10:32:19 +0900

    fix: log 삭제

--- scraping/PriceProcessor.py
+++ scraping/PriceProcessor.py
@@ -1,8 +1,6 @@
 class PriceProcessor:
     @staticmethod
     def proceed(str_price: str):
-        print('before process', str_price)
         str_price = str_price[:-1]
         res = int(''.join(str_price.split(',')))
-        print('res', res)
         return res
