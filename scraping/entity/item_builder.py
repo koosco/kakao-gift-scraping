99c6629da6e26a1411a27dbc69c1390022c26bcc
koosco <rnxogud123@naver.com>
2024-05-28 16:02:05 +0900

    refactor: builder가 django model 생성

--- scraping/entity/item_builder.py
+++ scraping/entity/item_builder.py
@@ -1,4 +1,7 @@
-from scraping.entity.item import Item
+import import_django
+
+from item_service.models import *
+
 from scraping.PriceProcessor import PriceProcessor
 
 from copy import deepcopy
@@ -8,32 +11,32 @@ class ItemBuilder:
     def __init__(self):
         self.__item = Item()
 
-    def item_image_url(self, image_url):
+    def item_image_url(self, image_url: str):
         self.__item.item_image_url = image_url
         return self
 
-    def brand_name(self, brand_name):
+    def brand_name(self, brand_name: str):
         self.__item.brand_name = brand_name
         return self
 
-    def item_name(self, item_name):
+    def item_name(self, item_name: str):
         self.__item.item_name = item_name
         return self
 
-    def price(self, str_price):
+    def price(self, str_price: str):
         price = PriceProcessor.proceed(str_price)
-        self.__item.price = price
+        self.__item.item_price = price
         return self
 
-    def category(self, category):
+    def category(self, category: str):
         self.__item.category = category
         return self
 
-    def sub_category(self, sub_category):
+    def sub_category(self, sub_category: str):
         self.__item.sub_category = sub_category
         return self
 
-    def option_name(self, option_name):
+    def option_name(self, option_name: str):
         self.__item.option_name = option_name
         return self
 
